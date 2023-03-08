3ff1219289ef55568221590c547823db46118646
DanielaNavalon <86865216+DanielaNavalon@users.noreply.github.com>
2023-03-07 20:19:31 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import './App.css';
+import Blog from './views/Blog';
+import ListadoPeliculas from './views/ListadoPeliculas';
+
+
+
+function App() { 
+
+
+  return (
+	<>
+	<BrowserRouter>
+	<Switch>
+		<Route path='/blog'>
+			<Blog />
+		</Route>
+		<Route path='/'>
+			<ListadoPeliculas />
+		</Route>
+	</Switch>
+	</BrowserRouter>
+	</>
+  );
+}
+
+export default App;
+
